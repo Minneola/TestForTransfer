4fe50abf5a919de2cf4e60c16a4c7730bc671963
Norbert Fox <nfox@dengophp.de>
2015-08-05 23:36:20 +0200

    Create Pod.php

---
+++ src/Support/Facades/Pod.php
@@ -0,0 +1,14 @@
+<?php
+namespace Minneola\TestFoo\Support\Facades;
+use Minneola\TestFoo\Support\Facade;
+/**
+ * Class Pod
+ * @package Minneola\TestFoo\Support\Facades
+ */
+class Pod extends Facade
+{
+	public static function getFacadeName()
+	{
+		return 'pod';
+	}
+} 
